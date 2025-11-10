f8e40dd7e4136ebaf65194c2f6c33c9c80809be8
fracispime <francisco.ponte.pimentel@student.nhlstenden.com>
2025-11-10 13:26:34 +0100

    basic

--- src/common/robot/movement/movement.h
+++ src/common/robot/movement/movement.h
@@ -1,6 +1,13 @@
 #pragma once
 
 void moveForward(int speed, int duration);
+void moveBackward(int speed, int duration);
+void moveLeft(int speed, int duration);
+void moveRight(int speed, int duration);
+
+void turnLeft();
+void turnRight();
+
 void moveStop();   
 
 
