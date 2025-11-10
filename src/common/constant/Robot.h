05ad0f90951a8a4fc835ee380363f4776f753c69
Yourgitname <youremail@student.nhlstenden.com>
2025-11-10 14:36:29 +0100

    Move Forward and Move Backwords functions

--- src/common/constant/Robot.h
+++ src/common/constant/Robot.h
@@ -3,6 +3,7 @@
  * @authors Sunny & Nicolo
  * @date 10-11-2025
  */
+#include <Arduino.h>
 
  // the pin of directions of wheels
 const int LEFT_DIRECTION_PIN1 = 5;
@@ -13,3 +14,23 @@ const int RIGHT_DIRECTIONL_PIN2 = 11;
 // pin of speed of wheels
 const int LEFT_SPEED_PIN = 3;
 const int RIGHT_SPEED_PIN = 9;
+
+// Move forward
+void moveForward(int speed, int duration) {
+  digitalWrite(LEFT_DIRECTION_PIN1, HIGH);
+  digitalWrite(RIGHT_DIRECTIONL_PIN1, HIGH);
+  analogWrite(LEFT_SPEED_PIN, speed);
+  analogWrite(RIGHT_SPEED_PIN, speed);
+  delay(duration);
+  stopMotors();
+}
+
+// Move backward
+void moveBackward(int speed, int duration) {
+  digitalWrite(LEFT_DIRECTION_PIN1, LOW);
+  digitalWrite(RIGHT_DIRECTIONL_PIN1, LOW);
+  analogWrite(LEFT_SPEED_PIN, speed);
+  analogWrite(RIGHT_SPEED_PIN, speed);
+  delay(duration);
+  stopMotors();
+}
