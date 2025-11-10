8e0c643425a6734ac6251073d740e7e983f8a12f
Sunny01 <whs3296116638@outlook.com>
2025-11-10 13:50:55 +0100

    add robot constants

---
+++ src/common/constant/Robot.h
@@ -0,0 +1,13 @@
+/**
+ * @name constants for robot
+ * @authors Sunny & Nicolo
+ * @date 10-11-2025
+ */
+
+ // the pin of directions of wheels
+const int LEFT_DIRECTION_PIN = 4;
+const int RIGHT_DIRECTIONL_PIN = 7;
+
+// pin of speed of wheels
+const int LEFT_SPEED_PIN = 3;
+const int RIGHT_SPEED_PIN = 5;
