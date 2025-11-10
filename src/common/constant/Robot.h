fc8a9727db4c580923204e63ecd7981aceccace4
Sunny01 <whs3296116638@outlook.com>
2025-11-10 14:13:20 +0100

    add some new constants in robot.h

--- src/common/constant/Robot.h
+++ src/common/constant/Robot.h
@@ -5,9 +5,11 @@
  */
 
  // the pin of directions of wheels
-const int LEFT_DIRECTION_PIN = 4;
-const int RIGHT_DIRECTIONL_PIN = 7;
+const int LEFT_DIRECTION_PIN1 = 5;
+const int LEFT_DIRECTION_PIN2 = 6;
+const int RIGHT_DIRECTIONL_PIN1 = 10;
+const int RIGHT_DIRECTIONL_PIN2 = 11;
 
 // pin of speed of wheels
 const int LEFT_SPEED_PIN = 3;
-const int RIGHT_SPEED_PIN = 5;
+const int RIGHT_SPEED_PIN = 9;
