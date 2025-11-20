c7f8c4c1932ac2257106ba8efef2756dbf43b3f3
Sunny01 <whs3296116638@outlook.com>
2025-11-20 21:42:59 +0100

    adjust the PID value for BB011

--- src/common/constant/motor.h
+++ src/common/constant/motor.h
@@ -8,40 +8,50 @@ const int PPR = 20;
 
 // ======================BB016======================
 
-const int PIN_MOTOR_RIGHT_FORWARD = 10;
-const int PIN_MOTOR_RIGHT_BACKWARD = 6;
+// const int PIN_MOTOR_RIGHT_FORWARD = 10;
+// const int PIN_MOTOR_RIGHT_BACKWARD = 6;
 
-const int PIN_MOTOR_LEFT_FORWARD = 5;
-const int PIN_MOTOR_LEFT_BACKWARD = 9;
+// const int PIN_MOTOR_LEFT_FORWARD = 5;
+// const int PIN_MOTOR_LEFT_BACKWARD = 9;
 
-const int PIN_MOTOR_LEFT_PULSE = 2;
-const int PIN_MOTOR_RIGHT_PULSE = 3;
+// const int PIN_MOTOR_LEFT_PULSE = 2;
+// const int PIN_MOTOR_RIGHT_PULSE = 3;
 
-// // PID factors
-const float Kp_f = 4.5; // Proportional
-const float Ki_f = 2.9; // Integral
-const float Kd_f = 0.2;  // Derivative
+// // // PID factors
+// const float Kp_f = 4.5; // Proportional
+// const float Ki_f = 2.9; // Integral
+// const float Kd_f = 0.2;  // Derivative
 
-const float Kp_b = 5.0;  
-const float Ki_b = 2.2;   
-const float Kd_b = 0.12; 
+// const float Kp_b = 5.0;  
+// const float Ki_b = 2.2;   
+// const float Kd_b = 0.12; 
 
-const float MOTOR_LEFT_FACTOR = 1.0;
-const float MOTOR_RIGHT_FACTOR = 0.95;
+// const float MOTOR_LEFT_FACTOR = 1.0;
+// const float MOTOR_RIGHT_FACTOR = 0.95;
 
 // ==================================================
 
 // ======================BB011======================
 
-// const int PIN_MOTOR_RIGHT_FORWARD = 9;
-// const int PIN_MOTOR_RIGHT_BACKWARD = 6;
+const int PIN_MOTOR_RIGHT_FORWARD = 11;
+const int PIN_MOTOR_RIGHT_BACKWARD = 10;
 
-// const int PIN_MOTOR_LEFT_FORWARD = 11;
-// const int PIN_MOTOR_LEFT_BACKWARD = 10;
+const int PIN_MOTOR_LEFT_FORWARD = 9;
+const int PIN_MOTOR_LEFT_BACKWARD = 6;
 
-// const int PIN_MOTOR_LEFT_PULSE = 2;
-// const int PIN_MOTOR_RIGHT_PULSE = 3;
+const int PIN_MOTOR_LEFT_PULSE = 2;
+const int PIN_MOTOR_RIGHT_PULSE = 3;
 
+const float Kp_f = 0.55; // Proportional
+const float Ki_f = 0.02; // Integral
+const float Kd_f = 0.1;  // Derivative
+
+const float Kp_b = 0.36;  
+const float Ki_b = 0.01;   
+const float Kd_b = 0.12; 
+
+const float MOTOR_LEFT_FACTOR = 1.0;
+const float MOTOR_RIGHT_FACTOR = 0.967;
 
 // ==================================================
 
