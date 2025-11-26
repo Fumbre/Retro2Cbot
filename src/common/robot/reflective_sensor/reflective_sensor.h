9fadcacddbf460d42420f80f72e9a2c2eddfb8af
Abyss <fumbre@mbp-abyss.wifi.nhlstenden.com>
2025-11-26 15:24:59 +0100

    test

--- src/common/robot/reflective_sensor/reflective_sensor.h
+++ src/common/robot/reflective_sensor/reflective_sensor.h
@@ -1,13 +1,3 @@
-/**
- * @name interface of reflective_sensor
- * @author Sunny
- * @date 21-11-2025
- * 
- */
 #pragma once
 #include <Arduino.h>
 #include "common/constant/reflective_sensor.h"
-
-void initReflectiveSensorsPins();
-
-MotorSpeed checkLine(int baseSpeed);
