d196ff69bb48038670f3e91542caec63430655ce
Sunny01 <whs3296116638@outlook.com>
2025-12-16 10:22:56 +0100

    maze line

--- src/maps_pogram/maze_line/maze_line.cpp
+++ src/maps_pogram/maze_line/maze_line.cpp
@@ -1,8 +1,29 @@
 #include "maze_line.h"
 
 // RS - reflective sensor
-ReflectiveSensor rsMaze(PINS_RS, PINS_RS_LENGTH, 150);
+ReflectiveSensor rsMaze(PINS_RS, PINS_RS_LENGTH, THRESHOLD);
 
-void mazeLine()
+void mazeLineSetup()
 {
+    setupMotor();
+    rsMaze.setup();
+}
+
+void mazeLine(int speed)
+{
+    uint8_t currentLineStatus = rsMaze.readBlackLine();
+    float pwmValue = getPWMValue(speed);
+    switch (currentLineStatus)
+    {
+    case CENTER:
+        moveSpeed(pwmValue, pwmValue);
+        break;   
+    }
+}
+
+float getPWMValue(int speed)
+{
+    speed = constrain(speed, 0, FULL_SPEED);
+    float pwmValue = (float)speed / FULL_SPEED * FULL_PWM_VALUE;
+    return constrain(pwmValue, 0, FULL_PWM_VALUE);
 }
--- src/maps_pogram/maze_line/maze_line.h
+++ src/maps_pogram/maze_line/maze_line.h
@@ -1,4 +1,9 @@
 #pragma once
 #include "common/tools/Reflective_sensor.h"
+#include "common/robot/movement/movement.h"
 
-void mazeLine();
+void mazeLineSetup();
+
+void mazeLine(int speed);
+
+float getPWMValue(int speed);
