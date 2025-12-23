2c004dc970778139e3be16e015264c1e28327df5
Sunny01 <whs3296116638@outlook.com>
2025-12-23 16:18:14 +0100

    add calibration and object avoidance in maze line

--- src/maps_pogram/maze_line/maze_line.h
+++ src/maps_pogram/maze_line/maze_line.h
@@ -3,6 +3,8 @@
 #include "common/robot/reflective_sensor/reflective_sensor.h"
 #include "common/robot/movement/movement.h"
 #include "common/tools/Timer.h"
+#include "common/robot/gripper/gripper.h"
+#include "common/robot/sonar/sonar.h"
 #include "common/tools/Start_sequence.h"
 
 void mazeLineSetup();
